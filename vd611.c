b6d15798e6ca502f23b5a4fe28b29ec624967218
your name <hoangckhg170@gmail.com>
2021-10-18 11:56:59 +0700

    Create vd611.c

---
+++ vd611.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i, n=0;
+	int item;
+	char x[10][12];
+	char temp[12];
+	printf("enter each string on a separate line\n\n");
+	printf("type'END'when over\n\n");
+	do
+	{
+		printf("string %d:",n+1);
+		scanf("%s",x[n]);
+	}while(strcmp(x[n++],"END"));
+	n=n-1;
+	for(item=0;item<n-1;++item)
+	{
+		for(i=item+1;i<n;++i)
+		{
+			if(strcmp(x[item],x[i])>0)
+			{
+				strcpy(temp,x[item]);
+				strcpy(x[item],x[i]);
+				strcpy(x[i],temp);
+			}
+		}
+	}
+	printf("record list of str :\n");
+	for(i=0;i<n;i++)
+	{
+		printf("\n str %d is %s",i+1,x[i]);
+	}
+}
