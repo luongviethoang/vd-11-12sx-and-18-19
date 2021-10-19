345b699dac1f3f26127aa35056c4f4b493401bd5
your name <hoangckhg170@gmail.com>
2021-10-19 22:56:25 +0700

    Create vd318.c

---
+++ vd318.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i;
+	char str_arr[10];
+	for(i=0;i<5;i++)
+	{
+		printf("\n Enter str %d:",i+1);
+		scanf("%s",&str_arr);
+	
+	}
+	return 0;
+}
