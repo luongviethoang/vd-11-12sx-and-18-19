49c948b0546d22735fdd13d1d8d8e2cb2a7a9be4
your name <hoangckhg170@gmail.com>
2021-10-19 22:56:33 +0700

    Create vd418.c

---
+++ vd418.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+for(i=0;i<4;i++)
+	for(j=i+1;j<5;j++)
+	{
+	if(strlen(str_arr[i]) < strlen(str_arr[j]))
+	{
+	strcpy(str, str_arr[i]);
+	strcpy(str_arr[i], str_arr[j]);
+	strcpy(str_arr[j], str);
+	}
+	return 0;
+}
