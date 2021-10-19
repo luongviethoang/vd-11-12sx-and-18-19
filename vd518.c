0baf6301c92ef553957f93063492692b9f38c917
your name <hoangckhg170@gmail.com>
2021-10-19 22:56:38 +0700

    Create vd518.c

---
+++ vd518.c
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+printf(“\nThe strings in descending order of length are:”);
+	for(i=0;i<5;i++)
+		printf(“\n%s”, str_arr[i]);
+	
+}
